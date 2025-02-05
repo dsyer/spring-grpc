6faf551e382b836e1b5c8ec9a0ee7236169c5da2
Dave Syer <dsyer@vmware.com>
2025-02-05 10:51:59 +0000

    Add autoconfiguration for clients

--- samples/grpc-server/src/test/java/org/springframework/grpc/sample/GrpcServerApplicationTests.java
+++ samples/grpc-server/src/test/java/org/springframework/grpc/sample/GrpcServerApplicationTests.java
@@ -8,23 +8,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.grpc.client.EnableGrpcClients;
-import org.springframework.grpc.client.GrpcClient;
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
 import org.springframework.grpc.sample.proto.HelloReply;
 import org.springframework.grpc.sample.proto.HelloRequest;
 import org.springframework.grpc.sample.proto.SimpleGrpc;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.grpc.client.default-channel.address=0.0.0.0:${local.grpc.port:9090}",
+		useMainMethod = UseMainMethod.ALWAYS)
 @DirtiesContext
-// @AutoConfigureInProcessTransport
 public class GrpcServerApplicationTests {
 
 	private static Log log = LogFactory.getLog(GrpcServerApplicationTests.class);
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(GrpcServerApplication.class, ExtraConfiguration.class).run();
+		new SpringApplicationBuilder(GrpcServerApplication.class).run();
 	}
 
 	@Autowired
@@ -39,12 +37,6 @@ public class GrpcServerApplicationTests {
 		log.info("Testing");
 		HelloReply response = stub.sayHello(HelloRequest.newBuilder().setName("Alien").build());
 		assertEquals("Hello ==> Alien", response.getMessage());
-	}
-
-	@TestConfiguration
-	@EnableGrpcClients(@GrpcClient(target = "0.0.0.0:${local.grpc.port:9090}", basePackageTypes = SimpleGrpc.class))
-	static class ExtraConfiguration {
-
 	}
 
 }
